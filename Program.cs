c75e6c9bc7ee1abb7edd9c74b6aff391b20d49b6
Marianna Fernandes <145359517+mariannalf@users.noreply.github.com>
2024-03-18 20:41:24 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a number to find its prime factors:");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine($"Prime factors of {number} are: {PrimeFactors(number)}");
+    }
+
+    static string PrimeFactors(int n)
+    {
+        string result = "";
+        int divisor = 2;
+
+        while (n > 1)
+        {
+            if (n % divisor == 0)
+            {
+                result += divisor + " ";
+                n /= divisor;
+            }
+            else
+            {
+                divisor++;
+            }
+        }
+
+        return result.Trim();
+    }
+}
